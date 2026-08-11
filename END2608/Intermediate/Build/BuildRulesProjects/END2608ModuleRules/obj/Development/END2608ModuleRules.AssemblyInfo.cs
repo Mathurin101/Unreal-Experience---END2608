5a72a96ce49c7d45757b07e80f98d95def133a96
Mathurin.G <mathuringenty101@gmail.com>
2026-08-11 18:18:14 -0400

    Opened unreal engine

--- END2608/Intermediate/Build/BuildRulesProjects/END2608ModuleRules/obj/Development/END2608ModuleRules.AssemblyInfo.cs
+++ END2608/Intermediate/Build/BuildRulesProjects/END2608ModuleRules/obj/Development/END2608ModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("END2608ModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e0fa940a45046b1ee96d0836e4e1ca725d509f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("END2608ModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("END2608ModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
